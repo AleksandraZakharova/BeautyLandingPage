d83e184fb4f0860d9b91999bd4909ab8c67b086a
Aleksandra Zakharova <alexkoz1996@gmail.com>
2021-11-07 22:50:20 +0700

    add browsersync dependencies and others

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,39 @@
-const { src, dest } = require('gulp');
+const { src, dest, series, watch, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
 function buildSass(){
     return src('src/scss/**/*.scss')
     .pipe(sass())
     .pipe(dest('dist/css'))
     .pipe(dest('src/css'))
+    .pipe(browserSync.stream())
 }
 
-exports.sass = buildSass;
+function buildHtml() {
+    return src('src/**/*.html').pipe(dest('dist')).pipe(browserSync.stream());
+}
+
+function copy() {
+    return src(['src/images/**/*.*', 'src/fonts/**/*.*'], { base: 'src'}).pipe(dest('dist'));
+}
+
+function cleanDist() {
+    return del('dist');
+}
+
+function serve() {
+    watch('src/scss/**/*.scss', buildSass);
+    watch('src/**/*.html', buildHtml);
+}
+
+function createDevServer() {
+    browserSync.init({
+        server: 'src',
+    //    notify: false
+    })
+}
+
+exports.build = series(cleanDist, buildSass, buildHtml, copy);
+exports.default = parallel(createDevServer, serve);
