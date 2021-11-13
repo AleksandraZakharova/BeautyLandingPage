2ab0acd7c99ca27c7b07b1c28051c5e5502bd74d
Aleksandra Zakharova <alexkoz1996@gmail.com>
2021-11-13 16:21:12 +0700

    add sourcemap configuration

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ const { src, dest, series, watch, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const sourcemap = require('gulp-sourcemaps')
 
 function buildSass(){
     return src('src/scss/**/*.scss')
+    .pipe(sourcemap.init())
     .pipe(sass())
+    .pipe(sourcemap.write())
     .pipe(dest('dist/css'))
     .pipe(dest('src/css'))
     .pipe(browserSync.stream())
