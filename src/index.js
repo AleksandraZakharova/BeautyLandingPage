ebaf2c39a577c259443a18ab5a9ba23a880d592e
Aleksandra Zakharova <alexkoz1996@gmail.com>
2021-12-04 16:33:01 +0700

    add nav

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+    document.getElementById("hamburger-nav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("hamburger-nav").style.height = "0%";
+}
