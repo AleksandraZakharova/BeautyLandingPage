5cb8f828fbefe7fbbe79efe295253ccaca904bf0
Aleksandra Zakharova <alexkoz1996@gmail.com>
2022-02-11 22:38:45 +0700

    output data from form to console

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,20 @@ function openNav() {
 function closeNav() {
     document.getElementById("hamburger-nav").style.height = "0%";
 }
+
+function submitForm() {
+    
+}
+
+function init() {
+    const recordForm = document.querySelector('.record-form');
+    recordForm.addEventListener('submit', event => {
+        event.preventDefault();
+        const {phone} = recordForm.elements;
+        const {name} = recordForm.elements;
+        console.log('phone: ', phone.value);
+        console.log('name: ', name.value);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', init)
