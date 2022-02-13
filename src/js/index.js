9746029f63fb9b6d15f1e32f8084014332c54109
Aleksandra Zakharova <alexkoz1996@gmail.com>
2022-02-13 18:55:00 +0700

    added validator

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,35 @@ import $ from 'jquery';
 import 'slick-carousel';
 import { Fancybox } from "@fancyapps/ui";
 import Inputmask from 'inputmask';
+import validate from 'jquery-validation'
 
 const $openDialogButton = $('#open-dialog-btn');
+const validateConfig = {
+    rules: {
+        name: {
+          required: true
+        },
+        phone : {
+            required: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Заполните имя"
+        },
+        phone: {
+            required: "Заполните телефон"
+        }
+      }
+  };
 
 $(document).ready(function(){
-    var selector = document.getElementsByName("phone")
-    var im = new Inputmask("+7 (999) 999-99-99");
+    const selector = document.getElementsByName("phone")
+    const im = new Inputmask("+7 (999) 999-99-99");
     im.mask(selector);
+
+    $(".dialog__form").validate(validateConfig);
+    $(".contacts-footer .record-form").validate(validateConfig);
 
     $('.hamburger').click(() => {
         document.getElementById("hamburger-nav").style.height = "100%";
