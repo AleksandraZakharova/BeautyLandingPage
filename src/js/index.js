a50a579015715f2a796c08cfc9da30758914b9b2
Aleksandra Zakharova <alexkoz1996@gmail.com>
2022-02-13 18:11:51 +0700

    added inputmask

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,14 @@
 import $ from 'jquery';
 import 'slick-carousel';
 import { Fancybox } from "@fancyapps/ui";
+import Inputmask from 'inputmask';
 
 const $openDialogButton = $('#open-dialog-btn');
 
 $(document).ready(function(){
+    var selector = document.getElementsByName("phone")
+    var im = new Inputmask("+7 (999) 999-99-99");
+    im.mask(selector);
 
     $('.hamburger').click(() => {
         document.getElementById("hamburger-nav").style.height = "100%";
